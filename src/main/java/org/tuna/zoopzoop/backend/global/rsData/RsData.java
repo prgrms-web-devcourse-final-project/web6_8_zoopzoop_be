eea7f75365f065c6b35eb43ed02b439b086f3c29
Whitedoggy <nubuli19@gmail.com>
2025-09-17 10:28:32 +0900

    feat: global 기초 스타터킷 완료.

--- src/main/java/org/tuna/zoopzoop/backend/global/rsData/RsData.java
+++ src/main/java/org/tuna/zoopzoop/backend/global/rsData/RsData.java
@@ -1,4 +1,17 @@
-package org.tuna.zoopzoop.backend.global.rsdata;
+package org.tuna.zoopzoop.backend.global.rsData;
 
-public class RsData {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public record RsData<T>(
+        String resultCode,
+        @JsonIgnore
+        int statusCode,
+        String msg,
+        T data
+) {
+    public RsData(String resultCode, String msg) { this(resultCode, msg, null); }
+
+    public RsData(String resultCode, String msg, T data) {
+        this(resultCode, Integer.parseInt(resultCode.split("-", 2)[0]), msg, data);
+    }
 }
--- src/main/java/org/tuna/zoopzoop/backend/global/security/SecurityConfig.java
+++ src/main/java/org/tuna/zoopzoop/backend/global/security/SecurityConfig.java
@@ -1,4 +1,45 @@
 package org.tuna.zoopzoop.backend.global.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 모든 요청 허용
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/favicon.ico",
+                                "/h2-console/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/api/v1/**" // API 테스트용으로 모두 허용.
+                        ).permitAll()
+                        .anyRequest().denyAll()
+                )
+
+                // CSRF 비활성화 (H2 콘솔 사용 위해 필요)
+                .csrf(csrf -> csrf.disable())
+
+                // H2 콘솔 사용 허용
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+
+                // 기본 인증 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable());
+
+        return http.build();
+    }
 }
