d651a1caebfe75cd9d9b8b1eff0e74123c08bb77
EpicFn <rlagurwls056@gmail.com>
2025-09-19 11:25:10 +0900

    refactor/OPS-222 : 테스트 클래스의 공용 파트 추상 클래스로 분리

--- src/test/java/org/tuna/zoopzoop/backend/domain/space/space/controller/ApiV1SpaceControllerTest.java
+++ src/test/java/org/tuna/zoopzoop/backend/domain/space/space/controller/ApiV1SpaceControllerTest.java
@@ -7,17 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import org.tuna.zoopzoop.backend.testSupport.ControllerTestSupport;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,11 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-class ApiV1SpaceControllerTest {
-    @Autowired
-    private MockMvc mvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+class ApiV1SpaceControllerTest extends ControllerTestSupport {
 
     // ============================= CREATE ============================= //
 
@@ -39,55 +30,7 @@ class ApiV1SpaceControllerTest {
     @DisplayName("스페이스 생성 - 성공")
     void createSpace_Success() {
 
-    }
 
-    // ========================== HELPER METHODS ========================== //
-
-
-    /** GET 요청 */
-    public ResultActions performGet(String url) throws Exception {
-        return mvc.perform(get(url)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
-    }
-
-    /** POST 요청 */
-    public ResultActions performPost(String url, Object body) throws Exception {
-        return mvc.perform(post(url)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(body)))
-                .andDo(print());
-    }
-
-    /** PUT 요청 */
-    public ResultActions performPut(String url, Object body) throws Exception {
-        return mvc.perform(put(url)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(body)))
-                .andDo(print());
-    }
-
-    /** PATCH 요청 */
-    public ResultActions performPatch(String url, Object body) throws Exception {
-        return mvc.perform(patch(url)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(body)))
-                .andDo(print());
-    }
-
-    /** DELETE 요청 */
-    public ResultActions performDelete(String url, Object body) throws Exception {
-        return mvc.perform(delete(url)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(body)))
-                .andDo(print());
-    }
-
-    /** DELETE 요청 (body 없는 경우) */
-    public ResultActions performDelete(String url) throws Exception {
-        return mvc.perform(delete(url)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
     }
 
 
@@ -107,37 +50,6 @@ class ApiV1SpaceControllerTest {
 //                List.of()
 //        );
 //    }
-
-
-    // ====================== COMMON ASSERTIONS ======================= //
-
-    private void expectForbidden(ResultActions resultActions) throws Exception {
-        resultActions.andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.code").value("403"))
-                .andExpect(jsonPath("$.message").value("권한이 없습니다."));
-    }
-
-//    private void expectClubCreated(ResultActions resultActions, Long expectedLeaderId) throws Exception {
-//        resultActions.andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.code").value(201))
-//                .andExpect(jsonPath("$.message").value("클럽이 생성됐습니다."))
-//                .andExpect(jsonPath("$.data.clubId").isNumber())
-//                .andExpect(jsonPath("$.data.leaderId").value(expectedLeaderId));
-//    }
-//
-//    private void expectClubUpdated(ResultActions resultActions, Long expectedClubId) throws Exception {
-//        resultActions.andExpect(status().isOk())
-//                .andExpect(jsonPath("$.code").value(200))
-//                .andExpect(jsonPath("$.message").value("클럽 정보가 수정됐습니다."))
-//                .andExpect(jsonPath("$.data.clubId").value(expectedClubId));
-//    }
-//
-//    private void expectNotFound(ResultActions resultActions, String message) throws Exception {
-//        resultActions.andExpect(status().isNotFound())
-//                .andExpect(jsonPath("$.code").value(404))
-//                .andExpect(jsonPath("$.message").value(message));
-//    }
-
 
 
 }
