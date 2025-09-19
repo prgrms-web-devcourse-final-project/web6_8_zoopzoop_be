372cc92531e53a58b103b54acbffc2def5999d9d
EpicFn <rlagurwls056@gmail.com>
2025-09-19 14:35:02 +0900

    feat/OPS-222 : 스페이스 생성 구현

--- src/main/java/org/tuna/zoopzoop/backend/domain/space/space/service/SpaceService.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/space/service/SpaceService.java
@@ -1,11 +1,27 @@
 package org.tuna.zoopzoop.backend.domain.space.space.service;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Service;
+import org.tuna.zoopzoop.backend.domain.space.space.entity.Space;
 import org.tuna.zoopzoop.backend.domain.space.space.repository.SpaceRepository;
 
 @Service
 @RequiredArgsConstructor
 public class SpaceService {
     private final SpaceRepository spaceRepository;
+
+    /**
+     * 스페이스 생성
+     * @param name 스페이스 이름
+     * @return 생성된 스페이스
+     */
+    public Space createSpace(@NotBlank @Length(max = 50) String name) {
+        Space newSpace = Space.builder()
+                .name(name)
+                .build();
+
+        return spaceRepository.save(newSpace);
+    }
 }
