8b83fae588d17ec7c03eeb40c377f8aad60ed269
EpicFn <rlagurwls056@gmail.com>
2025-09-19 10:39:03 +0900

    feat/OPS-222 : 스페이스 생성 엔드포인트 추가

--- src/main/java/org/tuna/zoopzoop/backend/domain/space/space/controller/ApiV1SpaceController.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/space/controller/ApiV1SpaceController.java
@@ -1,10 +1,14 @@
 package org.tuna.zoopzoop.backend.domain.space.space.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.tuna.zoopzoop.backend.domain.space.space.dto.SpaceCreateRequest;
+import org.tuna.zoopzoop.backend.domain.space.space.dto.SpaceCreateResponse;
 import org.tuna.zoopzoop.backend.domain.space.space.service.SpaceService;
+import org.tuna.zoopzoop.backend.global.rsData.RsData;
 
 @RestController
 @RequestMapping("/api/v1/space")
@@ -12,5 +16,17 @@ import org.tuna.zoopzoop.backend.domain.space.space.service.SpaceService;
 @Tag(name = "ApiV1SpaceController", description = "스페이스 관련 API")
 public class ApiV1SpaceController {
     private final SpaceService spaceService;
+
+    @PostMapping
+    @Operation(summary = "스페이스 생성")
+    public RsData<SpaceCreateResponse> createClub(
+            @Valid @RequestBody SpaceCreateRequest reqBody
+    ){
+        return new RsData<>("201", "클럽이 생성됐습니다.",
+                new SpaceCreateResponse(
+                        "buffer"
+                )
+        );
+    }
 
 }
---
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/space/dto/SpaceCreateResponse.java
@@ -0,0 +1,5 @@
+package org.tuna.zoopzoop.backend.domain.space.space.dto;
+
+public record SpaceCreateResponse(
+        String name
+) {}
