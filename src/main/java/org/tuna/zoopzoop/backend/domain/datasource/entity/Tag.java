e53aebe97f15d67723d8361471ee5c037139b5d4
ohsoohyuk <okzzangsh@gmail.com>
2025-09-22 11:20:04 +0900

    feat/OPS-151 : LLM 활용 데이터 정제화 기능 구현

--- src/main/java/org/tuna/zoopzoop/backend/domain/datasource/entity/Tag.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/datasource/entity/Tag.java
@@ -1,19 +1,19 @@
 package org.tuna.zoopzoop.backend.domain.datasource.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.tuna.zoopzoop.backend.global.jpa.entity.BaseEntity;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tag extends BaseEntity {
     //연결된 자료 id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "data_source_id", nullable = false)
+    @JoinColumn(name = "data_source_id", nullable = true) // BaseInitData 때문에 nullable true로 변경
     private DataSource dataSource;
 
     //태그명
