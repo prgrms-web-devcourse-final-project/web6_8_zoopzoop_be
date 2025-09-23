a7134565bc57ed48c99657bf17296f1341708f1a
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-09-23 12:04:02 +0900

    refactor/OPS-277 : default 폴더 자동 생성

--- src/main/java/org/tuna/zoopzoop/backend/domain/archive/archive/entity/Archive.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/archive/archive/entity/Archive.java
@@ -28,4 +28,20 @@ public class Archive extends BaseEntity {
     public Archive(ArchiveType archiveType) {
         this.archiveType = archiveType;
     }
+
+    public void addFolder(Folder folder) {
+        if (!this.folders.contains(folder)) {
+            this.folders.add(folder);
+        }
+        if (folder.getArchive() != this) {
+            folder.setArchive(this);
+        }
+    }
+
+    public void removeFolder(Folder folder) {
+        this.folders.remove(folder);
+        if (folder.getArchive() == this) {
+            folder.setArchive(null);
+        }
+    }
 }
