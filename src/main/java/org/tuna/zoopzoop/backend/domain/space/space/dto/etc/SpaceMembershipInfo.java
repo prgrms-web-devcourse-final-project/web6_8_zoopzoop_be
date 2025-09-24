30404f51054b4083f0eb4ae10908cd6150625a44
EpicFn <rlagurwls056@gmail.com>
2025-09-24 19:14:46 +0900

    refactor : space 도메인 dto 패키지 변경

--- src/main/java/org/tuna/zoopzoop/backend/domain/space/space/dto/etc/SpaceMembershipInfo.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/space/dto/etc/SpaceMembershipInfo.java
@@ -1,4 +1,4 @@
-package org.tuna.zoopzoop.backend.domain.space.space.dto;
+package org.tuna.zoopzoop.backend.domain.space.space.dto.etc;
 
 import org.tuna.zoopzoop.backend.domain.space.membership.enums.Authority;
 
