2edf688113480c05a21b44be1fbf61a36568c7d9
osh5030 <72571931+ohsoohyuk@users.noreply.github.com>
2025-09-25 15:07:52 +0900

    feat/OPS-289 : dataprocessor 서비스 생성 (#59)

--- src/main/java/org/tuna/zoopzoop/backend/domain/datasource/ai/dto/AiExtractorDto.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/datasource/ai/dto/AiExtractorDto.java
@@ -1,12 +1,12 @@
-package org.tuna.zoopzoop.backend.domain.datasource.dto;
+package org.tuna.zoopzoop.backend.domain.datasource.ai.dto;
 
 import java.time.LocalDate;
 
-public record ArticleData(
+public record AiExtractorDto(
         String title, // 제목
         LocalDate dataCreatedDate, // 작성일자
         String content, // ai한테 줘야할 내용
-        String imageUrl, // 이미지 url
+        String imageUrl, // 썸네일 이미지 url
         String sources // 출처
 ) {
 }
--- src/main/java/org/tuna/zoopzoop/backend/global/initData/BaseInitData.java
+++ src/main/java/org/tuna/zoopzoop/backend/global/initData/BaseInitData.java
@@ -54,13 +54,5 @@ public class BaseInitData {
         tagRepository.save(tag1);
         tagRepository.save(tag2);
         tagRepository.save(tag3);
-
-        aiService.summarizeAndTag("안녕 내 이름은 오수혁이야");
-        aiService.summarizeAndTag("D2Coding 1.3.2 버전을 릴리즈 합니다. ligature 관련 이슈를 수정하여, ligature 적용/미적용 폰트를 구분하여 배포합니다.\n" +
-                "\n" +
-                "기존 버전은 반드시 삭제후 설치 바랍니다.\n" +
-                "\n");
-
-        aiService.summarizeAndTag("Spring AI는 예외 발생 시 AiClientException, RetryableException 등으로 예외를 포장합니다. Spring의 기본 예외 처리 방식을 활용하면 에러 핸들링을 일관성 있게 구성할 수 있습니다.");
     }
 }
