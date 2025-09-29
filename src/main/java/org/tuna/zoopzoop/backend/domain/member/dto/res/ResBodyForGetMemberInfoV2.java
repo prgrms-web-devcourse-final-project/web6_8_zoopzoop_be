14a2fefc675028fe6a30da180e72ca9e87e38a70
Whitedoggy <nubuli19@gmail.com>
2025-09-29 11:11:00 +0900

    refactor/OPS-337 : 프론트 요구사항 반영 및 에러 수정.

--- src/main/java/org/tuna/zoopzoop/backend/domain/member/dto/res/ResBodyForGetMemberInfoV2.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/member/dto/res/ResBodyForGetMemberInfoV2.java
@@ -2,18 +2,22 @@ package org.tuna.zoopzoop.backend.domain.member.dto.res;
 
 import org.tuna.zoopzoop.backend.domain.member.entity.Member;
 
+import java.time.LocalDateTime;
+
 public record ResBodyForGetMemberInfoV2(
         Integer id,
         String name,
         String profileUrl,
-        String provider
+        String provider,
+        LocalDateTime createAt
 ) {
     public ResBodyForGetMemberInfoV2(Member member){
         this(
                 member.getId(),
                 member.getName(),
                 member.getProfileImageUrl(),
-                member.getProvider().name()
+                member.getProvider().name(),
+                member.getCreateDate()
         );
     }
 }
