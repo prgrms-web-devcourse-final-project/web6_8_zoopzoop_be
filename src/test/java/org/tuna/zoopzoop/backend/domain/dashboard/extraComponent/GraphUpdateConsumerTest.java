05c4f78e5803fd885cbdaf10113c8599a6e2d597
EpicFn <rlagurwls056@gmail.com>
2025-09-30 17:30:15 +0900

    fix : 테스트 코드 수정 중

--- src/test/java/org/tuna/zoopzoop/backend/domain/dashboard/extraComponent/GraphUpdateConsumerTest.java
+++ src/test/java/org/tuna/zoopzoop/backend/domain/dashboard/extraComponent/GraphUpdateConsumerTest.java
@@ -1,16 +1,16 @@
 package org.tuna.zoopzoop.backend.domain.dashboard.extraComponent;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.AbstractStringAssert;
+import org.junit.jupiter.api.*;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.tuna.zoopzoop.backend.domain.dashboard.dto.GraphUpdateMessage;
 import org.tuna.zoopzoop.backend.domain.dashboard.entity.Graph;
-import org.tuna.zoopzoop.backend.domain.dashboard.repository.DashboardRepository;
 import org.tuna.zoopzoop.backend.domain.space.space.entity.Space;
 import org.tuna.zoopzoop.backend.domain.space.space.service.SpaceService;
 import org.tuna.zoopzoop.backend.testSupport.ControllerTestSupport;
@@ -22,30 +22,29 @@ import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GraphUpdateConsumerTest extends ControllerTestSupport {
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    private DashboardRepository dashboardRepository;
-
-    @Autowired
-    SpaceService spaceService;
+    @Autowired private RabbitTemplate rabbitTemplate;
+    @Autowired private TransactionTemplate transactionTemplate;
+    @Autowired SpaceService spaceService;
 
     // 테스트에 사용할 dashboardId (실제 DB에 존재하는 ID)
     private Integer existingDashboardId;
 
-    @BeforeEach
+    private String existingSpaceName = "TestSpace1_forGraphUpdateConsumerTest";
+
+    @BeforeAll
     void setUp(){
-        spaceService.createSpace("TestSpace1_forGraphUpdateConsumerTest", "thumb1");
+        spaceService.createSpace(existingSpaceName, "thumb1");
     }
+
 
     @Test
     @DisplayName("큐에 업데이트 메시지가 들어오면 컨슈머가 DB를 성공적으로 업데이트한다")
     void handleGraphUpdate_Success() throws Exception {
         // Given
         // ControllerTestSupport의 setUp에서 생성된 대시보드 ID를 가져옵니다.
-        existingDashboardId = spaceService.findByName("TestSpace1_forGraphUpdateConsumerTest").getDashboard().getId();
+        existingDashboardId = spaceService.findByName(existingSpaceName).getDashboard().getId();
 
         String requestBody = createReactFlowJsonBody(); // 테스트용 JSON 데이터
         GraphUpdateMessage message = new GraphUpdateMessage(existingDashboardId, requestBody);
@@ -55,9 +54,14 @@ class GraphUpdateConsumerTest extends ControllerTestSupport {
 
         // Then: 컨슈머가 메시지를 처리하여 DB가 변경될 때까지 최대 5초간 기다립니다.
         await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
-            Graph updatedGraph = dashboardRepository.findById(existingDashboardId).get().getGraph();
-            assertThat(updatedGraph.getNodes()).hasSize(2);
-            assertThat(updatedGraph.getNodes().get(0).getData().get("title")).isEqualTo("노드1");
+            transactionTemplate.execute(status -> {
+                Space space = spaceService.findByName(existingSpaceName);
+                Graph updatedGraph = space.getDashboard().getGraph();
+                assertThat(updatedGraph.getNodes()).hasSize(2);
+                assertThat(updatedGraph.getEdges()).hasSize(1);
+                assertThat(updatedGraph.getNodes().get(0).getData().get("title")).isEqualTo("노드1");
+                return null;
+            });
         });
     }
 
