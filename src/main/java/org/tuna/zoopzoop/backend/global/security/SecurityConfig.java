51648bdffe2cfea9235a20594e2584859091efe2
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2025-09-30 14:47:39 +0900

    fix/OPS-362 : 요구사항 반영#6 (#100)

--- src/main/java/org/tuna/zoopzoop/backend/global/security/SecurityConfig.java
+++ src/main/java/org/tuna/zoopzoop/backend/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.tuna.zoopzoop.backend.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,6 +31,7 @@ public class SecurityConfig {
 
                 // 모든 요청 허용
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/v1/member", "/api/v1/member/**").authenticated()
                         .requestMatchers(
                                 "/",
