b8a65c3245f827c3629f0ce8debcb6bfe4c0a5c3
EpicFn <rlagurwls056@gmail.com>
2025-09-30 12:05:33 +0900

    new : RabbitMQ 설정 클래스, dto 생성

---
+++ src/main/java/org/tuna/zoopzoop/backend/global/mq/RabbitMQConfig.java
@@ -0,0 +1,47 @@
+package org.tuna.zoopzoop.backend.global.mq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private static final String EXCHANGE_NAME = "zoopzoop.exchange";
+    private static final String QUEUE_NAME = "graph.update.queue";
+    private static final String ROUTING_KEY = "graph.update.#";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        // 메시지를 JSON 형식으로 직렬화/역직렬화하는 컨버터
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
