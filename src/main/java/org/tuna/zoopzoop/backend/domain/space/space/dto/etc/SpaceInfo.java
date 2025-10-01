552140a755bd1266297330db9bc905b8fdd914d4
EpicFn <rlagurwls056@gmail.com>
2025-10-01 17:49:34 +0900

    feat : 구현 완료

--- src/main/java/org/tuna/zoopzoop/backend/domain/space/space/dto/etc/SpaceInfo.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/space/dto/etc/SpaceInfo.java
@@ -1,11 +1,20 @@
 package org.tuna.zoopzoop.backend.domain.space.space.dto.etc;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.tuna.zoopzoop.backend.domain.space.membership.dto.etc.SpaceMemberInfo;
 import org.tuna.zoopzoop.backend.domain.space.membership.enums.Authority;
 
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record SpaceInfo(
         Integer id,
         String name,
         String thumbnailUrl,
-        Authority authority
+        Authority authority,
+        List<SpaceMemberInfo> members
 ) {
+    public SpaceInfo(Integer id, String name, String thumbnailUrl, Authority authority) {
+        this(id, name, thumbnailUrl, authority, null);
+    }
 }
