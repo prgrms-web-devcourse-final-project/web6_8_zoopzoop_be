4b746a1d105aeb394df61d6c17d591e85bb76a83
Hyeok Jin Kim <71928299+EpicFn@users.noreply.github.com>
2025-10-02 10:27:53 +0900

    feat : 스페이스 목록 조회 시 생성 일자도 반환 (#114)
    Co-authored-by: EpicFn <rlagurwls056@gmail.com>

--- src/main/java/org/tuna/zoopzoop/backend/domain/space/membership/dto/etc/SpaceMemberInfo.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/space/membership/dto/etc/SpaceMemberInfo.java
@@ -2,6 +2,8 @@ package org.tuna.zoopzoop.backend.domain.space.membership.dto.etc;
 
 import org.tuna.zoopzoop.backend.domain.space.membership.enums.Authority;
 
+import java.time.LocalDateTime;
+
 public record SpaceMemberInfo(
         Integer id,
         String name,
