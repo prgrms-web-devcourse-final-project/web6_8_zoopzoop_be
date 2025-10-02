5159ba462bf0f760ea8358726215d4f083b68415
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2025-10-02 11:50:43 +0900

    [feat/OPS-377] 운영/테스트 서버 분리 작업 (#115)
    * feat/OPS-377 : 테스트/운영 환경 분리
    
    * feat/OPS-377 : 테스트/운영 환경 분리 #2
    
    * feat/OPS-377 : 테스트/운영 환경 분리 #3
    
    * feat/OPS-377 : 테스트/운영 환경 분리 #4

--- src/main/java/org/tuna/zoopzoop/backend/domain/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/auth/handler/OAuth2SuccessHandler.java
@@ -41,8 +41,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Value("${front.redirect_domain}")
     private String redirect_domain;
 
-    @Value("${spring.profiles.active:dev}")
-    private String activeProfile;
+    private final String SITE_DOMAIN = "zoopzoop.kro.kr";
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -112,7 +111,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                     .maxAge(jwtProperties.getAccessTokenValidity() / 1000)
                     // .domain() // 프론트엔드 & 백엔드 상위 도메인
                     // .secure(true) // https 필수 설정.
-                    .domain(redirect_domain)
+                    .domain(SITE_DOMAIN)
                     .secure(true)
                     .sameSite("None")
                     .build();
@@ -121,7 +120,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                     .httpOnly(true)
                     .path("/")
                     .maxAge(jwtProperties.getRefreshTokenValidity() / 1000) // RefreshToken 유효기간과 동일하게
-                    .domain(redirect_domain)
+                    .domain(SITE_DOMAIN)
                     .secure(true)
                     .sameSite("None")
                     .build();
