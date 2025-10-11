ca84e33adc39ef949d269140025de58d5352babe
Whitedoggy <nubuli19@gmail.com>
2025-10-11 14:27:32 +0900

    fix/OPS-394: API 문서화 및 s3 오류 수정 #4

--- src/main/java/org/tuna/zoopzoop/backend/domain/member/dto/res/ResBodyForSearchMember.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/member/dto/res/ResBodyForSearchMember.java
@@ -4,13 +4,15 @@ import org.tuna.zoopzoop.backend.domain.member.entity.MemberDocument;
 
 public record ResBodyForSearchMember(
     int id,
-    String name
+    String name,
+    String profileImageUrl
 ) {
-    public ResBodyForSearchMember(int id, String name) {
+    public ResBodyForSearchMember(int id, String name, String profileImageUrl) {
         this.id = id;
         this.name = name;
+        this.profileImageUrl = profileImageUrl;
     }
-    public ResBodyForSearchMember(MemberDocument memberDocument){
-        this(memberDocument.getId(), memberDocument.getName());
+    public ResBodyForSearchMember(MemberDocument memberDocument, String profileImageUrl){
+        this(memberDocument.getId(), memberDocument.getName(), profileImageUrl);
     }
 }
